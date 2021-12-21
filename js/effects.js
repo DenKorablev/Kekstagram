745610efd399cba180502ea67d4f720d4910e2f4
DenKorablev <denis18051990@mail.ru>
2021-12-21 12:39:53 +0300

    Доработал эффекты

--- js/effects.js
+++ js/effects.js
@@ -1,8 +1,12 @@
+const START_VALUE = 40;
+
 const SliderParams = {
   MIN: 0,
   MAX: 100,
   STEP: 1,
 }
+
+let currentPictureClass = '';
 
 const image = document.querySelector('.img-upload__preview img');
 
@@ -12,54 +16,32 @@ const sliderLevel = document.querySelector('.effect-level__slider');
 const sliderValue = document.querySelector('.effect-level__value');
 const effectNone = document.querySelector('#effect-none');
 
-let currentPictureClass = '';
 
 const effects = {
-  chrome: {
-    min: 0,
-    max: 1,
-    setFilter: (value) => `grayscale(${value})`
-  },
-  sepia: {
-    min: 0,
-    max: 1,
-    setFilter: (value) => `sepia(${value})`
-  },
-  marvin: {
-    min: 0,
-    max: 100,
-    setFilter: (value) => `invert(${value}%)`
-  },
-  phobos: {
-    min: 0,
-    max: 3,
-    setFilter: (value) => `blur(${value}px)`
-  },
-  heat: {
-    min: 1,
-    max: 3,
-    setFilter: (value) => `brightness(${value})`
-  },
-  none: {
-    min: 0,
-    max: 0,
-    setFilter: () => `none`
-  }
+  chrome: (value) => `grayscale(${parseInt(value, 10) * 0.01})`,
+  sepia: (value) => `sepia(${parseInt(value, 10) * 0.01 })`,
+  marvin: (value) => `invert(${parseInt(value, 10) }%)`,
+  phobos: (value) => `blur(${(parseInt(value, 10) * 3) * 0.01 }px)`,
+  heat: (value) => `brightness(${(parseInt(value, 10) * 3) * 0.01 })`,
+  none: () => 'none',
 };
 
 const onAddEffect = (evt) => {
   const value = evt.target.value;
-  sliderLevel.noUiSlider.set(100);
-  if (currentPictureClass) {
-    image.classList.remove(currentPictureClass);
-  }
   if (value) {
+    if (currentPictureClass) {
+      image.classList.remove(currentPictureClass);
+    }
     currentPictureClass = `effects__preview--${value}`
     image.classList.add(currentPictureClass);
-    sliderContainer.style.display = effectNone.checked ? 'none' : 'block';
-    debugger
-    image.style.filter = effects[value].setFilter(parseInt(sliderValue.value));
+    setSliderEffect(value);
   }
+}
+
+const setSliderEffect = (value) => {
+  sliderContainer.style.display = effectNone.checked ? 'none' : 'block';
+  sliderLevel.noUiSlider.set(START_VALUE);
+  image.style.filter = effects[value](START_VALUE);
 }
 
 export const initEffects = () => {
@@ -83,6 +65,6 @@ window.noUiSlider.create(sliderLevel, {
 
 sliderLevel.noUiSlider.on('change', (evt) => {
   const value = currentPictureClass.replace('effects__preview--', '');
-  sliderValue.value = sliderLevel.noUiSlider.get();
-  image.style.filter = effects[value].setFilter(parseInt(sliderValue.value));
+  sliderValue.value = evt[0];
+  image.style.filter = effects[value](sliderValue.value);
 })
